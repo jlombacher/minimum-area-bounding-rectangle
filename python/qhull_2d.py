a28a9b85f06bdd767ef0726ec0036046bf99542b
Jakob Lombacher <Jakob@Lombacher.net>
2014-06-23 09:19:24 +0200

    Merge branch 'master' of https://github.com/jlombacher/minimum-area-bounding-rectangle

--- python/qhull_2d.py
+++ python/qhull_2d.py
@@ -25,7 +25,7 @@
 # FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS 
 # IN THE SOFTWARE.
 
-from __future__ import division
+
 from numpy import *
 
 link = lambda a,b: concatenate((a,b[1:]))
